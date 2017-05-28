f0469c2603dd6fd7a95e670929345b53cba0a58a
1iveowl <liveowl@outlook.com>
2017-05-28 09:44:54 +0200

    wip

---
+++ src/interface/IParallelTaskQueueRx/IParallelTaskQueueRx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace IParallelTaskQueueRx
+{
+    public interface IParallelTaskQueueRx<TReturnValue>
+    {
+        IObservable<TReturnValue> ObservableResults { get; }
+
+        void ProcessTaskOnMyQueue(
+            Func<Task<TReturnValue>> myTask,
+            TReturnValue errorReturnValue,
+            string queueId);
+    }
+}
---
+++ src/main/ParallelTaskQueueRx/ParallelTaskQueueRx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IParallelTaskQueueRx;
+
+namespace ParallelTaskQueueRx
+{
+    public class ParallelTaskQueueRx<TReturnValue> : IParallelTaskQueueRx<TReturnValue>
+    {
+        public IObservable<TReturnValue> ObservableResults => throw new NotImplementedException();
+
+        public void ProcessTaskOnMyQueue(
+            Func<System.Threading.Tasks.Task<TReturnValue>> myTask, 
+            TReturnValue errorReturnValue, 
+            string queueId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
