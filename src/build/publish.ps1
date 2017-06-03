11b2815c7feb60df6f1854b44546b8bb5a10e8e1
1iveowl <liveowl@outlook.com>
2017-06-03 12:52:24 +0200

    wip

--- src/build/publish.ps1
+++ src/build/publish.ps1
@@ -9,6 +9,6 @@ else {
 	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\interface\IParallelTaskQueueRx\bin\Release\netstandard1.2\IParallelTaskQueueRx.dll')).Version.ToString(3) + "-" + $betaver
 }
 
-NuGet.exe pack SimpleHttpServer.nuspec -Verbosity detailed -Symbols -OutputDir "NuGet" -Version $version
+NuGet.exe pack ParallelTaskQueueRx.nuspec -Verbosity detailed -Symbols -OutputDir "NuGet" -Version $version
 
 Nuget.exe push .\Nuget\ParallelTaskQueueRx.nuspec.$version.nupkg -Source https://www.nuget.org
